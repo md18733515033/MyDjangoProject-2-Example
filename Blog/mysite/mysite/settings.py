b4262335e2d797140fd4a47cfaf0811d70c1c7a0
mengd <sjzxjmd@163.com>
2019-09-01 23:18:43 +0800

    mysql config

--- Blog/mysite/mysite/settings.py
+++ Blog/mysite/mysite/settings.py
@@ -75,8 +75,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': "root",
+        'PASSWORD': "123456",
+        'HOST': "127.0.0.1",
+        'PORT': 3306,
+        'TEST': {
+            'NAME': 'test_core2db',
+            'CHARSET': 'utf8mb4',
+            'COLLATION': 'utf8mb4_general_ci'
+        },
     }
 }
 
