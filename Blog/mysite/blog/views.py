da229037a3370e7f579955a785383503c54003c9
mengd <sjzxjmd@163.com>
2019-09-08 21:24:29 +0800

    添加使用基于类的视图

--- Blog/mysite/blog/views.py
+++ Blog/mysite/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
@@ -19,3 +21,10 @@ def post_list(request):
 def post_detail(request, year, month, day, post):
     post = Post.objects.get(slug=post, status='published', publish__year=year)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
